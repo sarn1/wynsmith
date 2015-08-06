8704a1d5b4ad9239a6f12412102d171ae51c2d3a
Webvolution <spham@webvolutionchicago.com>
2015-08-06 17:32:47 -0500

    main template & tweaks

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
   <section>
     <div id="content_body">
       <?php if (have_posts()) :
-        while (have_posts()) : the_post(); ?>
-
-          <h1><?php the_title(); ?></h1>
-          <article><?php the_content(); ?></article>
-
-          <?php get_sidebar(); ?>
-
+        while (have_posts()) : the_post();
+        //default full width template
+      ?>
+        <div class="row">
+          <div class="medium-12 columns">
+            <h1><?php the_title(); ?></h1>
+            <article><?php the_content(); ?></article>
+          </div>
+        </div>
         <?php endwhile;
       elseif (is_404()) : ?>
         <div class="row">
